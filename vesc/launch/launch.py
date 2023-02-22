d12c485ba568ba0221bcf6f3e48d2eed18f6c64c
Chaim Maier (FDTech) <chaim.maier@fdtech.de>
2023-02-22 18:06:07 +0100

    fixed misplaced brackets

--- vesc/launch/launch.py
+++ vesc/launch/launch.py
@@ -17,12 +17,12 @@ def generate_launch_description():
 
     components.append(launch.actions.IncludeLaunchDescription(
         XMLLaunchDescriptionSource(os.path.join(get_package_share_directory(
-            "vesc_ackermann"), 'launch', "vesc_to_odom_node.launch.xml"), launch_arguments=params.items()),
+            "vesc_ackermann"), 'launch', "vesc_to_odom_node.launch.xml")), launch_arguments=params.items(),
     ))
 
     components.append(launch.actions.IncludeLaunchDescription(
         XMLLaunchDescriptionSource(os.path.join(get_package_share_directory(
-            "vesc_ackermann"), 'launch', "ackermann_to_vesc_node.launch.xml"), launch_arguments=params.items()),
+            "vesc_ackermann"), 'launch', "ackermann_to_vesc_node.launch.xml")), launch_arguments=params.items(),
     ))
 
     components.append(launch.actions.IncludeLaunchDescription(
