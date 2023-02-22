adaee7e3e4569097733e84e1dd8dfa7eef91517a
Chaim Maier (FDTech) <chaim.maier@fdtech.de>
2023-02-22 18:10:46 +0100

    make parameters externally tunable

--- vesc/launch/launch.py
+++ vesc/launch/launch.py
@@ -1,17 +1,31 @@
 import os
 import launch
-from launch_ros.actions import Node
+from launch_ros.actions import DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import TextSubstitution
 
 from pathlib import Path
 
 
 def generate_launch_description():
 
-    params = {"speed_to_erpm_gain": "4614.0", "speed_to_erpm_offset": "0.0",
-              "steering_angle_to_servo_gain": "-1.2135", "steering_angle_to_servo_offset": "0.5304"}
+    speed_to_erpm_gain = DeclareLaunchArgument(
+        "speed_to_erpm_gain", default_value=TextSubstitution(text="4614.0")
+    )
+    speed_to_erpm_offset = DeclareLaunchArgument(
+        "speed_to_erpm_offset", default_value=TextSubstitution(text="0.0")
+    )
+    steering_angle_to_servo_gain = DeclareLaunchArgument(
+        "steering_angle_to_servo_gain", default_value=TextSubstitution(text="-1.2135")
+    )
+    steering_angle_to_servo_offset = DeclareLaunchArgument(
+        "steering_angle_to_servo_offset", default_value=TextSubstitution(text="0.5304")
+    )
+
+    params = {"speed_to_erpm_gain": speed_to_erpm_gain, "speed_to_erpm_offset": speed_to_erpm_offset,
+              "steering_angle_to_servo_gain": steering_angle_to_servo_gain, "steering_angle_to_servo_offset": steering_angle_to_servo_offset}
 
     components = []
 
